f4791a6b37fdc001d452e6012a6855df04d05eca
andytilia <andytilia@users.noreply.github.com>
2017-05-11 21:33:40 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,11 @@
+var img;
+
 function setup() {
   createCanvas(400, 400);
+  img = loadImage("https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Albert_Einstein_Head.jpg/768px-Albert_Einstein_Head.jpg");
 }
 
 function draw() {
   ellipse(80, 80, 80, 80);
+  image(img, 0, height/2, img.width/2, img.height/2);)
 }
